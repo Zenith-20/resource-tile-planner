4c0f555626a16ba8a7b417b6c678995a3ee84809
Zenith-20 <102802742+Zenith-20@users.noreply.github.com>
2026-08-09 11:07:20 +0500

    docs(useEditor): add initial documentation

--- src/hooks/useEditor.ts
+++ src/hooks/useEditor.ts
@@ -1,6 +1,12 @@
 import { useContext } from "react"
 import { EditorContext } from "../contexts/EditorContext"
+import { EditorProvider } from "../contexts/EditorProvider"
 
+/**
+ * Wrapper hook that allows editor context to be used
+ *
+ * Throws an error if this hook is used by a component outside the {@link EditorProvider} component
+ */
 export function useEditor(){
     const editorContext = useContext(EditorContext)
     if (editorContext == null) throw new Error("Null context")
